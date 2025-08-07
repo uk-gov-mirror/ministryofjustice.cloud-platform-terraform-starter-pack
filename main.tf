a0e83a54fc166d00ca3e60422b454d20ad81aef5
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-08-07 12:42:09 +0100

    fix: wip

--- main.tf
+++ main.tf
@@ -136,12 +136,12 @@ module "secret" {
     },
   }
 
-  team_name              = var.team_name
-  application            = var.application
-  business_unit          = var.business_unit
-  is_production          = var.is_production
-  namespace              = var.namespace
-  environment_name       = var.environment
-  infrastructure_support = var.infrastructure_support
-  eks_cluster_name       = var.eks_cluster_name
+  team_name              = "test"
+  application            = "test"
+  business_unit          = "test"
+  is_production          = "no"
+  namespace              = "starter-pack-0"
+  environment_name       = "test"
+  infrastructure_support = "test"
+  eks_cluster_name       = "live"
 }
