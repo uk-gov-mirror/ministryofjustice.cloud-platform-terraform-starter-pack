126f0b8a1a2dd0b42785312089c1ac6e21009388
Tim Cheung <152907271+timckt@users.noreply.github.com>
2025-08-07 12:26:39 +0100

    chore: add secret to starter pack

--- main.tf
+++ main.tf
@@ -121,3 +121,20 @@ resource "helm_release" "multi_container_app" {
   ]
 }
 
+module "secret" {
+  source = "github.com/ministryofjustice/cloud-platform-terraform-secrets-manager?ref=3.0.4" # use the latest release
+
+  # EKS configuration
+  eks_cluster_name = var.eks_cluster_name
+
+  # Secrets configuration
+  secrets = {
+    "test" = {
+      description             = "test secret" # required
+      recovery_window_in_days = 7                # required
+      k8s_secret_name         = "test-secret" # the name of the secret in k8s
+    },
+  }
+
+
+}
